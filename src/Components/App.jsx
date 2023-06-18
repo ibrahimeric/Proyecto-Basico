7c8d253c9f9b693d6ac65c7ed6c8a08f02a1b8cd
eric <ericibrahim19@gmail.com>
2023-06-18 16:09:15 -0300

    carousel

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "./Header";
-import Principal from './Principal';
-import Footer from './Footer';
+import Header from "./Header.jsx";
+import Carousel from "./Carousel.jsx";
+import Principal from './Principal.jsx';
+import Footer from './Footer.jsx';
 
 function App() {
+  const items = ['Elemento 1', 'Elemento 2', 'Elemento 3', 'Elemento 4'];
+
   return (
     <div>
       <Header></Header>
+      <Carousel></Carousel>
       <Principal></Principal>
       <Footer></Footer>
     </div>
