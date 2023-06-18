bcd9e48bfb61e66740b5ab397d18e68ad3a158f9
eric <ericibrahim19@gmail.com>
2023-06-18 15:26:08 -0300

    formulario

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
 import Principal from './Principal';
 import Footer from './Footer';
--- src/Components/Principal.jsx
+++ src/Components/Principal.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import '../Styles/Principal.css'
+import React from 'react';
+import '../Styles/Principal.css';
 import Boton from './Boton.jsx';
 import Imagenes from './Imagenes.jsx';
+import Form from '../Components/Form.jsx';
 
 const Principal = () => {
     return (
         <div className='contenido-principal'>
             <Boton></Boton>
             <Imagenes></Imagenes>
+            <Form></Form>
         </div>
     )
 }
