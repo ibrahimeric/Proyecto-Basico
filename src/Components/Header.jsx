b557e3827d67a841d6ab4cfa8c555f1b4e639e78
eric <ericibrahim19@gmail.com>
2023-06-18 01:52:08 -0300

    logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Img512 from '../img/logo512.png'
 
 const Header = () => {
     return (
+        <>
         <div>
             <h1>hola</h1>
         </div>
+        <img
+            src={Img512}
+        >
+        </img>
+        </>
     )
 }
 
